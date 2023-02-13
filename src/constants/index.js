ed88c94ee00c501e6af4144e4f282b7e521d1c78
Qayumov Pahlavon <qayumovpahlavon2778tatu@gmail.com>
2023-02-13 18:37:00 +0500

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,141 @@
+export const allItems = [
+  {
+    id: "100",
+    title: "All Departments",
+  },
+
+  {
+    id: "101",
+    title: "Arts & Crafts",
+  },
+
+  {
+    id: "102",
+    title: "Automotive",
+  },
+
+  {
+    id: "103",
+    title: "Baby",
+  },
+
+  {
+    id: "104",
+    title: "Beauty & Personal Care",
+  },
+
+  {
+    id: "105",
+    title: "Books",
+  },
+
+  {
+    id: "106",
+    title: "Boys' Fashion",
+  },
+
+  {
+    id: "107",
+    title: "Computers",
+  },
+
+  {
+    id: "108",
+    title: "Deals",
+  },
+
+  {
+    id: "109",
+    title: "Digital Music",
+  },
+
+  {
+    id: "110",
+    title: "Electronics",
+  },
+
+  {
+    id: "111",
+    title: "Girls' Fashion",
+  },
+
+  {
+    id: "112",
+    title: "Health & Household",
+  },
+
+  {
+    id: "113",
+    title: "Home & Kitchen",
+  },
+
+  {
+    id: "114",
+    title: "Industrial & Scientific",
+  },
+
+  {
+    id: "115",
+    title: "Kindle Store",
+  },
+
+  {
+    id: "116",
+    title: "Luggage",
+  },
+
+  {
+    id: "117",
+    title: "Men's Fashion",
+  },
+
+  {
+    id: "118",
+    title: "Movies & TV",
+  },
+
+  {
+    id: "119",
+    title: "Music, CDs & Vinyl",
+  },
+
+  {
+    id: "120",
+    title: "Pet Supplies",
+  },
+
+  {
+    id: "121",
+    title: "Prime Video",
+  },
+
+  {
+    id: "122",
+    title: "Software",
+  },
+
+  {
+    id: "123",
+    title: "Sports & Outdoors",
+  },
+
+  {
+    id: "124",
+    title: "Tools & Home Improvement",
+  },
+
+  {
+    id: "125",
+    title: "Toys & Games",
+  },
+
+  {
+    id: "126",
+    title: "Video Games",
+  },
+
+  {
+    id: "127",
+    title: "Women's Fashion",
+  },
+];
