0fe9c330ef758383cd038e28ffe5c622e54c13a3
Qayumov Pahlavon <qayumovpahlavon2778tatu@gmail.com>
2023-02-13 19:22:05 +0500

    Added home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/header/Header";
+import Banner from "./components/home/Banner";
 
 function App() {
   return (
     <div className="font-bodyFont">
       <Header />
+      <Banner />
     </div>
   );
 }
