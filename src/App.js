8b51d4266caa596a373858901403f16cfb4e017b
Qayumov Pahlavon <qayumovpahlavon2778tatu@gmail.com>
2023-02-14 11:28:50 +0500

    Added footer components and react-router added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Outlet,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
-import Banner from "./components/home/Banner";
+// import Banner from "./components/home/Banner";
+import Home from "./pages/Home";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />}></Route>
+      </Route>
+    )
+  );
+
   return (
     <div className="font-bodyFont">
-      <Header />
-      <Banner />
+      <RouterProvider router={router}>
+        {/* <Header />
+        <Banner />
+        <Footer /> */}
+      </RouterProvider>
     </div>
   );
 }
